324ffbb29ad53735ec51fbf3fb5cbbeb1fc27706
Leonid Turnaev <lmars@mail.ru>
2013-04-09 21:39:07 +0700

    Implement error "Additional parameter to bean constructor added"

--- Calculator/src/ru/lmars/calculator/Calculator.java
+++ Calculator/src/ru/lmars/calculator/Calculator.java
@@ -2,17 +2,20 @@ package ru.lmars.calculator;
 
 public class Calculator
 {
-	public Calculator(Operation operation, ResultWriter resultWriter)
+	public Calculator(Operation operation, ResultWriter resultWriter, Logger logger)
 	{
 		this.operation = operation;
 		this.resultWriter = resultWriter;
+		this.logger = logger;
 	}
 	
 	public void Run(int left, int right)
 	{
+		logger.LogMessage("Start calculation");
 		resultWriter.Write(operation.Run(left, right));
 	}
 	
 	private Operation operation;
 	private ResultWriter resultWriter;
+	private Logger logger;
 }
