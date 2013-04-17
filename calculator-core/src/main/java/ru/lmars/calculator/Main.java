6a354902225abb57bb377050bfbc03f523e2dcbf
Leonid Turnaev <lmars@mail.ru>
2013-04-17 16:44:53 +0700

    Fix run issue

--- calculator-core/src/main/java/ru/lmars/calculator/Main.java
+++ calculator-core/src/main/java/ru/lmars/calculator/Main.java
@@ -1,13 +1,14 @@
 package ru.lmars.calculator;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
-		ApplicationContext applicationContext = new FileSystemXmlApplicationContext("/beans.xml");
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
 		
 		Calculator calculator = applicationContext.getBean(Calculator.class);
 		calculator.Run(1, 2);
